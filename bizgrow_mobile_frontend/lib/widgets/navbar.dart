c28b25ad05ccdf219f1917e90b69c87aa64059bc
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2024-10-18 14:52:31 +0700

    add logic selected item in navbar

--- bizgrow_mobile_frontend/lib/widgets/navbar.dart
+++ bizgrow_mobile_frontend/lib/widgets/navbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:bizgrow_mobile_frontend/styles/colors.dart';
 
 class MainNavigator extends StatefulWidget {
   @override
@@ -18,34 +19,48 @@ class _MainNavigatorState extends State<MainNavigator> {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
-      // backgroundColor: Colors.(#151436),
+      backgroundColor: MainColors.darkBlue,
+      selectedItemColor: Monochrome.whiteDarkMode,
+      unselectedItemColor: Monochrome.lightGrey,
       currentIndex: _selectedIndex,
       onTap: _onItemTapped,
       items: [
         BottomNavigationBarItem(
           icon: ImageIcon(
-            AssetImage('lib/assets/navbar_icon/default/Beranda.png'),
+            AssetImage(
+              _selectedIndex == 0? 
+              'lib/assets/navbar_icon/selected/Beranda.png' : 'lib/assets/navbar_icon/default/Beranda.png',
+            ),
             size: 24,
           ),
           label: 'Beranda',
         ),
         BottomNavigationBarItem(
           icon: ImageIcon(
-            AssetImage('lib/assets/navbar_icon/default/Penjualan.png'),
+            AssetImage(
+              _selectedIndex == 1? 
+              'lib/assets/navbar_icon/selected/Penjualan.png' : 'lib/assets/navbar_icon/default/Penjualan.png',
+            ),
             size: 24,
           ),
           label: 'Penjualan',
         ),
         BottomNavigationBarItem(
           icon: ImageIcon(
-            AssetImage('lib/assets/navbar_icon/default/Stok.png'),
+            AssetImage(
+              _selectedIndex == 2? 
+              'lib/assets/navbar_icon/selected/Stok.png' : 'lib/assets/navbar_icon/default/Stok.png',
+            ),
             size: 24,
           ),
           label: 'Stok',
         ),
         BottomNavigationBarItem(
           icon: ImageIcon(
-            AssetImage('lib/assets/navbar_icon/default/Profil.png'),
+            AssetImage(
+              _selectedIndex == 3? 
+              'lib/assets/navbar_icon/selected/Profil.png' : 'lib/assets/navbar_icon/default/Profil.png',
+            ),
             size: 24,
           ),
           label: 'Profil',
