1719d2b5977b9e09060d62ea02aa30b8d8707eda
ahmadfadliakbar <afka.akbar@gmail.com>
2025-01-08 14:56:26 +0700

    [FIX] Commit sementara

--- bizgrow_mobile_frontend/lib/screens/beranda/beranda_screen.dart
+++ bizgrow_mobile_frontend/lib/screens/beranda/beranda_screen.dart
@@ -3,7 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:bizgrow_mobile_frontend/themes/colors.dart';
 import 'package:bizgrow_mobile_frontend/themes/theme.dart';
 import 'package:bizgrow_mobile_frontend/themes/text_styles.dart';
-import 'package:bizgrow_mobile_frontend/widgets/navbar.dart'; // Removed FontAwesome import
+import 'package:bizgrow_mobile_frontend/widgets/navbar.dart';
+import 'package:bizgrow_mobile_frontend/screens/penjualan/input_data_penjualan.dart';
 
 class BerandaScreen extends StatelessWidget {
   const BerandaScreen({super.key});
@@ -21,69 +22,115 @@ class BerandaScreen extends StatelessWidget {
           children: [
             // Greeting text
             Text(
-              'Selamat datang, Nama',
+              'Selamat datang, Nama UMKM',
               style: GoogleFonts.montserrat(
-                fontSize: 16,
+                fontSize: 18,
                 color: Monochrome.whiteDarkMode,
                 letterSpacing: -0.5,
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 20),
 
-            // Status message box
-            Container(
-              padding: EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 13, 1, 107),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Semua berjalan dengan baik',
-                    style: GoogleFonts.montserrat(
-                      fontSize: 18,
-                      color: Colors.white,
+            // Total Penjualan
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Total Penjualan'),
+                      content: Text('Total penjualan yang ditampilkan adalah untuk bulan ini.'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Tutup'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 13, 1, 107),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total Penjualan',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  Icon(
-                    Icons.check_circle,
-                    color: Colors.greenAccent,
-                    size: 24,
-                  ),
-                ],
+                    SizedBox(height: 10),
+                    Text(
+                      'Rp 700.000,00',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 28,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             SizedBox(height: 20),
 
-            // Profit section
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 13, 1, 107),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Profit minggu ini',
-                    style: GoogleFonts.montserrat(
-                      fontSize: 16,
-                      color: Colors.white,
+            // Total Pembelian
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Total Pembelian'),
+                      content: Text('Total pembelian yang ditampilkan adalah untuk bulan ini.'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Tutup'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 13, 1, 107),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total Pembelian',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Rp700.000,00',
-                    style: GoogleFonts.montserrat(
-                      fontSize: 28,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                    SizedBox(height: 10),
+                    Text(
+                      'Rp 500.000,00',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 28,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             SizedBox(height: 20),
@@ -97,39 +144,39 @@ class BerandaScreen extends StatelessWidget {
                 children: [
                   _buildQuickAccessButton(
                     context,
-                    Icons
-                        .people, // Replaced FontAwesome with standard Flutter icon
+                    Icons.people,
                     'Demand',
+                    InputDataPenjualanScreen(),
                   ),
                   _buildQuickAccessButton(
                     context,
-                    Icons
-                        .inventory, // Replaced FontAwesome with standard Flutter icon
+                    Icons.inventory,
                     'Buffer Stock',
+                    InputDataPenjualanScreen(),
                   ),
                   _buildQuickAccessButton(
                     context,
-                    Icons
-                        .show_chart, // Replaced FontAwesome with standard Flutter icon
+                    Icons.show_chart,
                     'Profit',
+                    InputDataPenjualanScreen(),
                   ),
                   _buildQuickAccessButton(
                     context,
-                    Icons
-                        .history, // Replaced FontAwesome with standard Flutter icon
+                    Icons.history,
                     'Riwayat Penjualan',
+                    InputDataPenjualanScreen(),
                   ),
                   _buildQuickAccessButton(
                     context,
-                    Icons
-                        .list_alt, // Replaced FontAwesome with standard Flutter icon
+                    Icons.list_alt,
                     'Riwayat Stok',
+                    InputDataPenjualanScreen(),
                   ),
                   _buildQuickAccessButton(
                     context,
-                    Icons
-                        .upload, // Replaced FontAwesome with standard Flutter icon
+                    Icons.upload,
                     'Input Data Penjualan',
+                    InputDataPenjualanScreen(),
                   ),
                 ],
               ),
@@ -143,30 +190,42 @@ class BerandaScreen extends StatelessWidget {
 
   // Helper function to create Quick Access Buttons
   Widget _buildQuickAccessButton(
-      BuildContext context, IconData icon, String label) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 13, 1, 107),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            icon,
-            color: Colors.white,
-            size: 32,
-          ),
-          SizedBox(height: 8),
-          Text(
-            label,
-            style: GoogleFonts.montserrat(
-              fontSize: 14,
+    BuildContext context,
+    IconData icon,
+    String label,
+    Widget targetScreen,
+  ) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => targetScreen),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 13, 1, 107),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
               color: Colors.white,
+              size: 32,
             ),
-          ),
-        ],
+            SizedBox(height: 8),
+            Text(
+              label,
+              style: GoogleFonts.montserrat(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
