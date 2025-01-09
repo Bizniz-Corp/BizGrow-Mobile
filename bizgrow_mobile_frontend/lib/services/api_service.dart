0b2e98905795d101c491ad439ccfe322f9bb8d7e
Syauqi <syauqi.d.u@gmail.com>
2025-01-10 06:56:54 +0700

    [FIX] Benerin URL jadi local IP

--- bizgrow_mobile_frontend/lib/services/api_service.dart
+++ bizgrow_mobile_frontend/lib/services/api_service.dart
@@ -7,7 +7,8 @@ import 'package:bizgrow_mobile_frontend/models/product.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  final String baseUrl = "http://10.0.2.2:8000/api";
+  // final String baseUrl = "http://10.0.2.2:8000/api";
+  final String baseUrl = "http://192.168.1.15:8000/api";
 
   Future<void> setToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
